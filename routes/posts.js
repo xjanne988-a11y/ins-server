61e2f72ce848637a02acb6eb0e3563fd1e48a0f8
xjanne988-a11y <xjanne988@gmail.com>
2026-06-23 18:13:39 +0800

    Add routes/posts.js

---
+++ routes/posts.js
@@ -0,0 +1,11 @@
+﻿const express = require("express");
+const data = require("../data");
+const router = express.Router();
+
+router.get("/:shortcode", (req, res) => {
+  const post = data.getPost(req.params.shortcode);
+  if (!post) return res.status(404).json({ error: "Post not found" });
+  res.json({ post });
+});
+
+module.exports = router;
