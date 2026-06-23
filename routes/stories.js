85bdba5d66cc5660bbb2ed2fe154d036b0af01e8
xjanne988-a11y <xjanne988@gmail.com>
2026-06-23 18:13:45 +0800

    Add routes/stories.js

---
+++ routes/stories.js
@@ -0,0 +1,12 @@
+﻿const express = require("express");
+const data = require("../data");
+const router = express.Router();
+
+router.get("/:username", (req, res) => {
+  const blogger = data.getBlogger(req.params.username);
+  if (!blogger) return res.status(404).json({ error: "Blogger not found" });
+  const stories = data.getStoriesByBlogger(blogger.id);
+  res.json({ stories });
+});
+
+module.exports = router;
